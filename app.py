c7bce23d3a3a75cb9aa91d07989117b474f15391
Venu_DevOps <venugopalreddy1322@gmail.com>
2025-06-18 19:49:55 +0200

    updates to project

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     #return 'Hello world!!!'
-    return render_template('index1.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
